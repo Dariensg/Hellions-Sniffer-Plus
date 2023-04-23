e1c73ec23ea7ae273a3cc22bb92026ed92f0c72b
Darien Gillespie <dsgillespie02@yahoo.com>
2023-04-23 18:47:52 +0200

    Tweak chest rendering to follow animations

--- Common/src/main/java/com/helliongames/snifferplus/client/renderer/entity/layers/SnifferChestLayer.java
+++ Common/src/main/java/com/helliongames/snifferplus/client/renderer/entity/layers/SnifferChestLayer.java
@@ -2,20 +2,25 @@ package com.helliongames.snifferplus.client.renderer.entity.layers;
 
 import com.helliongames.snifferplus.access.SnifferAccess;
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
 import net.minecraft.client.model.SnifferModel;
+import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.animal.sniffer.Sniffer;
 import net.minecraft.world.level.block.Blocks;
 
 public class SnifferChestLayer extends RenderLayer<Sniffer, SnifferModel<Sniffer>> {
     private final BlockRenderDispatcher blockRenderer;
+    private final SnifferModel<Sniffer> model;
 
     public SnifferChestLayer(RenderLayerParent<Sniffer, SnifferModel<Sniffer>> renderLayerParent, BlockRenderDispatcher blockRenderDispatcher) {
         super(renderLayerParent);
+        this.model = renderLayerParent.getModel();
         this.blockRenderer = blockRenderDispatcher;
     }
 
@@ -25,8 +30,13 @@ public class SnifferChestLayer extends RenderLayer<Sniffer, SnifferModel<Sniffer
             return;
         }
         poseStack.pushPose();
+        ModelPart body = model.root().getChild("bone").getChild("body");
+        poseStack.translate(body.x / 20, body.y / 20, body.z / 20);
         poseStack.translate(0.5f, 0.25f, 1.25f);
         poseStack.scale(-1.0f, -0.5f, 0.5f);
+        poseStack.mulPose(Axis.XP.rotationDegrees(body.xRot * Mth.RAD_TO_DEG));
+        poseStack.mulPose(Axis.YP.rotationDegrees(body.yRot * Mth.RAD_TO_DEG));
+        poseStack.mulPose(Axis.ZP.rotationDegrees(body.zRot * Mth.RAD_TO_DEG));
         this.blockRenderer.renderSingleBlock(Blocks.CHEST.defaultBlockState(), poseStack, multiBufferSource, i, OverlayTexture.NO_OVERLAY);
         poseStack.popPose();
     }
