a4c6a398618d18b0df4ca1e22a28aadabb554ad3
HellionGames <dsgillespie02@yahoo.com>
2023-06-10 18:36:26 -0400

    Fix Forge sign textures

--- Forge/src/main/java/com/helliongames/snifferplus/platform/ForgeWoodTypeHelper.java
+++ Forge/src/main/java/com/helliongames/snifferplus/platform/ForgeWoodTypeHelper.java
@@ -1,6 +1,8 @@
 package com.helliongames.snifferplus.platform;
 
+import com.helliongames.snifferplus.Constants;
 import com.helliongames.snifferplus.platform.services.IWoodTypeHelper;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
@@ -9,6 +11,6 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 public class ForgeWoodTypeHelper implements IWoodTypeHelper {
     @Override
     public WoodType registerWoodType(String identifier, BlockSetType setType, SoundType soundType, SoundType hangingSignSoundType, SoundEvent fenceGateClose, SoundEvent fenceGateOpen) {
-        return WoodType.register(new WoodType(identifier, setType, soundType, hangingSignSoundType, fenceGateClose, fenceGateOpen));
+        return WoodType.register(new WoodType(new ResourceLocation(Constants.MOD_ID, identifier).toString(), setType, soundType, hangingSignSoundType, fenceGateClose, fenceGateOpen));
     }
 }
